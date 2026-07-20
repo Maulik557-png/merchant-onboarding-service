e678a6031b73ad81ebed3e1ab89aaeea24883c5b
smaulik557-admin <smaulik557@gmail.com>
2026-07-20 18:22:18 +0530

    Created a fresh project from start.spring.io - Tested in postman by addition controller - Added needed dependencies

---
+++ src/main/java/com/paybridge/onboard/controller/AdditionController.java
@@ -0,0 +1,15 @@
+package com.paybridge.onboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdditionController {
+	
+	@GetMapping("/add")
+	public int add(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+	}
+	
+}
